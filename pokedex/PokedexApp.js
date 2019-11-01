22655297da739eaac3a6778edc8637e35c4bb107
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-11-01 10:34:56 -0700

    rendering all hard coded HTML

--- pokedex/PokedexApp.js
+++ pokedex/PokedexApp.js
@@ -5,7 +5,7 @@ import SearchType from './SearchType.js';
 import Sort from './Sort.js';
 import Filter from './Filter.js';
 import PokemonList from './PokemonList.js';
-import PokemonCard from './PokemonCard.js';
+// import PokemonCard from './PokemonCard.js';
 import Pagination from './Pagination.js';
 
 class PokedexApp extends Component{
@@ -14,13 +14,35 @@ class PokedexApp extends Component{
         const header = new Header();
         dom.prepend(header.renderDOM());
 
-        const searchName = new SearchName
+        const searchNameDiv = dom.querySelector('#search-name');
+        const searchName = new SearchName();
+        searchNameDiv.appendChild(searchName.renderDOM());
+
+        const searchTypeDiv = dom.querySelector('#search-type');
+        const searchType = new SearchType();
+        searchTypeDiv.appendChild(searchType.renderDOM());
+
+        const sortDiv = dom.querySelector('#sort-by');
+        const sort = new Sort();
+        sortDiv.appendChild(sort.renderDOM());
+
+        const filterDiv = dom.querySelector('#filter-by-height');
+        const filter = new Filter();
+        filterDiv.appendChild(filter.renderDOM());
+
+        const pokemonListDiv = dom.querySelector('#pokemon-list-div');
+        const pokemonList = new PokemonList();
+        pokemonListDiv.appendChild(pokemonList.renderDOM());
+
+        const footer = dom.querySelector('#footer');
+        const pagination = new Pagination();
+        footer.appendChild(pagination.renderDOM());
 
     }
     
     renderHTML(){
         return `
-        <div>        <main>
+        <div><main>
         <div id="search-name">
         </div>
         <div id="search-type">
@@ -32,7 +54,7 @@ class PokedexApp extends Component{
         <div id="pokemon-list-div">
         </div>
     </main>
-    <footer>
+    <footer id="footer">
     </footer></div>
         `;
     }
